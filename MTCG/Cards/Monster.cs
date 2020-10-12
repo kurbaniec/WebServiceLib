f33a438d07d4ab82fbb709427a828775fdd898e4
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-10-12 20:09:30 +0200

    Added monster definitions

--- MTCG/Cards/Monster.cs
+++ MTCG/Cards/Monster.cs
@@ -2,6 +2,12 @@
 {
     public abstract class Monster : Card
     {
-
+        protected Monster(string name, uint damage, ElementType type, uint elementDamage)
+        {
+            this._name = name;
+            this._damage = damage;
+            this._type = type;
+            this._elementDamage = elementDamage;
+        }
     }
 }
---
+++ MTCG/Cards/Monsters/Knight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace MTCG.Cards.Monsters
+{
+    public class Knight : Monster
+    {
+        public override bool IsResistant(Card enemyCard)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsWeak(Card enemyCard)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool CanEvade(Card enemyCard)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override uint CalculateDamage(List<Card> enemyCard)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
