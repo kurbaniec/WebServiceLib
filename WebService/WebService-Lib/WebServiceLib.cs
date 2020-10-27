381e500e5c244ced7836e722f5019d8575cc6981
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-10-27 12:20:32 +0100

    Started work on WebService 🚀 Using Assembly class to get program assemblies to scan later for rest api definitions.

--- WebService/WebService-Lib/WebServiceLib.cs
+++ WebService/WebService-Lib/WebServiceLib.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace WebService_Lib
 {
     public class SimpleWebService
     {
+        private Assembly programAssembly;
+
+        public SimpleWebService(Assembly programAssembly)
+        {
+            this.programAssembly = programAssembly;
+        }
+
         public void Start()
         {
+            // Get program Assemblies
+            // See: https://stackoverflow.com/a/1315687/12347616
             Console.WriteLine("WebService has started...");
+            foreach (Type type in programAssembly.GetTypes())
+            {
+                Console.WriteLine(type.FullName);
+            }
         }
     }
 }
