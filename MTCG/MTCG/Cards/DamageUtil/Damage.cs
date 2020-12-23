e3fba9392e10d2e21420d59bf8d761b1aa41bab8
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-12-23 12:14:32 +0100

    Continuing work on MTCG Added Damage class for Card Damage comparisons.

---
+++ MTCG/MTCG/Cards/DamageUtil/Damage.cs
@@ -0,0 +1,45 @@
+﻿namespace MTCG.Cards.DamageUtil
+{
+    public class Damage : IDamage
+    {
+        private bool infty;
+        private uint value;
+        public bool IsInfty => infty;
+        public uint Value => value;
+
+        private Damage(bool infty, uint value)
+        {
+            this.infty = infty;
+            this.value = value;
+        }
+
+        public static Damage Normal(uint value) => new Damage(false, value);
+        
+        public static Damage Infty() => new Damage(true, 0);
+
+        public void Add(uint damage)
+        {
+            if (!infty) value += damage;
+        }
+
+        public void SetInfty()
+        {
+            if (!infty) infty = true;
+        }
+
+        public void SetNoDamage()
+        {
+            if (infty) infty = false;
+            value = 0;
+        }
+        
+        public int CompareTo(IDamage? other)
+        {
+            if (other == null) return 1;
+            if ((this.IsInfty && other.IsInfty) || (this.Value == other.Value)) return 0;
+            if ((this.IsInfty && !other.IsInfty) ||
+                ((!this.IsInfty && !other.IsInfty) && (this.Value > other.Value))) return 1;
+            return -1;
+        }
+    }
+}
---
+++ MTCG/MTCG/Cards/DamageUtil/IDamage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MTCG.Cards.DamageUtil
+{
+    public interface IDamage : IComparable<IDamage>
+    {
+        bool IsInfty { get; }
+        uint Value { get; }
+        void Add(uint damage);
+        void SetInfty();
+        void SetNoDamage();
+    }
+}
